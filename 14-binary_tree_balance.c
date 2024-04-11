2857fdf246c586355c39b8ecf5e128a4475998c0
Abdel-lhamid <abdelhamidbusiness@gmail.com>
2024-04-11 20:48:22 +0000

    fix betty

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -32,6 +32,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	size_t r_h = 0, l_h = 0;
+
 	if (!tree)
 		return (0);
 	if (tree->right)
