1618746d3e19a100d743f0d905fc3e69229325d3
Abdel-lhamid <abdelhamidbusiness@gmail.com>
2024-04-11 15:25:45 +0000

    fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,6 +13,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_n;
 
+	if (parent == NULL)
+		return (NULL);
 	new_n = binary_tree_node(parent, value);
 
 	if (new_n == NULL)
