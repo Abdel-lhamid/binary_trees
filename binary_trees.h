2d3fa533b1235233ae61c6fc5f7d9ec1399bfb0f
ACHERKI-YOUSSEF <youssef.acherki@um5r.ac.ma>
2024-04-11 17:49:21 -0700

    task18

--- binary_trees.h
+++ binary_trees.h
@@ -45,5 +45,6 @@ int pow_recur(int x, int y);
 size_t tree_size(const binary_tree_t *tree);
 size_t tree_height(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 #endif /* BINARY_TREES */
